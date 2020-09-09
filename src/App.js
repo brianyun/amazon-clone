01ba3fb2f34bd63473676776bab82a2fdda62419
윤희상 <huisang.growth@gmail.com>
2020-09-09 10:48:14 +0900

    header styled

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import "./App.css";
 import Header from "./Header";
 
 function App() {
-	//BEM convention (className="app")
 	return (
 		<div className="app">
-			{/* header */}
-			{/* home */}
 			<Header />
 		</div>
 	);
--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,39 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 const Header = () => {
 	return (
 		<div className="header">
-			<div>
-				<img src="/amazon-logo-transparent.png" alt="Amazon" />
+			<img
+				className="header__logo"
+				src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+				alt="Amazon"
+			/>
+			<div className="header__search">
+				<input className="header__searchInput" type="text" />
+				<SearchIcon className="header__searchIcon" />
 			</div>
-			<div>
-				<input />
-			</div>
-			<div>
-				<button>Sign In</button>
-			</div>
-			<div>
-				<button>& Orders</button>
-			</div>
-			<div>
-				<button>Prime</button>
-			</div>
-			<div>
-				<button>Basket</button>
+			<div className="header__nav">
+				<div className="header__option">
+					<span className="header__optionLineOne">Hello Guest</span>
+					<span className="header__optionLineTwo">Sign In</span>
+				</div>
+				<div className="header__option">
+					<span className="header__optionLineOne">Returns</span>
+					<span className="header__optionLineTwo">& Orders</span>
+				</div>
+				<div className="header__option">
+					<span className="header__optionLineOne">Your</span>
+					<span className="header__optionLineTwo">Prime</span>
+				</div>
+				<div className="header__optionBasket">
+					<ShoppingBasketIcon />
+					<span className="header__optionLineTwo header__basketCount">
+						0
+					</span>
+				</div>
 			</div>
 		</div>
 	);
