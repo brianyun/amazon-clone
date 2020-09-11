ecb3024b8116b06a1e5738e22b47558e3858ed12
윤희상 <huisang.growth@gmail.com>
2020-09-11 16:25:17 +0900

    firebase deployed, front & back

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "http://localhost:5001/clone-690bc/us-central1/api/",
 	//the API (cloud function) URL
+	// baseURL: "http://localhost:5001/clone-690bc/us-central1/api/",
+	baseURL: "https://us-central1-clone-690bc.cloudfunctions.net/api",
 });
 
 export default instance;
